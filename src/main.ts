73e692ed463a235f198007d22bece390caaf9440
stanovikov <stanovikov@ozon.ru>
2023-01-18 16:22:41 +0700

    сохранение

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,14 @@ import {Container} from 'inversify';
 import {Component} from './types/component.types.js';
 import {LoggerInterface} from './common/logger/logger.interface.js';
 import {ConfigInterface} from './common/config/config.interface.js';
+import {DatabaseInterface} from './common/database-client/database.interface.js';
+import DatabaseService from './common/database-client/database.service.js';
 
 const applicationContainer = new Container();
 applicationContainer.bind<Application>(Component.Application).to(Application).inSingletonScope();
 applicationContainer.bind<LoggerInterface>(Component.LoggerInterface).to(LoggerService).inSingletonScope();
 applicationContainer.bind<ConfigInterface>(Component.ConfigInterface).to(ConfigService).inSingletonScope();
+applicationContainer.bind<DatabaseInterface>(Component.DatabaseInterface).to(DatabaseService).inSingletonScope();
 
 
 const application = applicationContainer.get<Application>(Component.Application);
